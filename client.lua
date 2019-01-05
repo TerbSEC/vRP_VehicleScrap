a51d5c3468da8dfb84cc2a82c2bbf4f345ea5e31
Terbium <39789348+TerbSEC@users.noreply.github.com>
2019-01-05 20:37:17 +0000

    Update client.lua

--- client.lua
+++ client.lua
@@ -16,9 +16,6 @@ scrapyards = {
 }
 
 
-
-
-
 --------------------------------------------------------------------
       -- >> DRAWMARKER AND THINGS LIKE THAT FUNCTION
 --------------------------------------------------------------------
@@ -28,38 +25,34 @@ Citizen.CreateThread(function()
 		Citizen.Wait(0)
 		for i = 1, #scrapyards do
 			scrapCoords2 = scrapyards[i] --
-			if GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), scrapCoords2[1], scrapCoords2[2], scrapCoords2[3], true ) < 40 then
-	   			if IsPedSittingInAnyVehicle(GetPlayerPed(-1)) then
+			if GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), scrapCoords2[1], scrapCoords2[2], scrapCoords2[3], true ) < 40 then
+	   			if IsPedSittingInAnyVehicle(PlayerPedId()) then
 					DrawMarker(27, scrapCoords2[1], scrapCoords2[2], scrapCoords2[3], 0, 0, 0, 0, 0, 0, 5.75, 5.75, 5.75, 53, 146, 0, 100, 0, 0, 0, 1)
-					if GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), scrapCoords2[1], scrapCoords2[2], scrapCoords2[3], true ) < 3 then
-						vehSR_drawTxt("Press ~g~E~s~ to scratch your vehicle",0,1,0.5,0.95,0.6,255,255,255,255)
+					if GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), scrapCoords2[1], scrapCoords2[2], scrapCoords2[3], true ) < 3 then
+						drawTxt("Press ~g~E~s~ to scratch your vehicle",0,1,0.5,0.95,0.6,255,255,255,255)
 						if IsControlJustPressed(1, key) then
-							local veh = GetVehiclePedIsUsing(GetPlayerPed(-1), false)
-            						if (GetPedInVehicleSeat(veh, -1) == GetPlayerPed(-1)) then
-								local vehicle1 = GetVehiclePedIsIn(GetPlayerPed(-1), false)
-								local plate1 = GetVehicleNumberPlateText(vehicle1)
-								if string.sub(plate1, 1, 8) == "P " .. vRP.getRegistrationNumber({}) then
+							local veh = GetVehiclePedIsIn(GetPlayerPed(-1), false)
+							if (GetPedInVehicleSeat(veh, -1) == PlayerPedId()) then
+								local plate = GetVehicleNumberPlateText(veh)
+								if string.sub(plate, 1, 8) == "P " .. vRP.getRegistrationNumber({}) then
 									TriggerEvent("pNotify:SendNotification",{text = "You can not scratch your own vehicle!",type = "error",timeout = (1500),layout = "centerRight",queue = "global",animation = {open = "gta_effects_fade_in", close = "gta_effects_fade_out"}})
 								else
 									if vehPrice ~= nil then
-										local health = GetEntityHealth(GetVehiclePedIsIn(GetPlayerPed(-1)))
+										local health = GetEntityHealth(GetVehiclePedIsIn(PlayerPedId()))
 										local modifier = health/1000
-										modPrice = round((modifier * vehPrice), 0)
+										modPrice = math.floor((modifier * vehPrice), 0)
 									else
-										local vehicle = GetVehiclePedIsIn(GetPlayerPed(-1), false)
-										local class = GetVehicleClass(vehicle)
-										TriggerServerEvent("scrap:getVehPrice", class)
+										local class = GetVehicleClass(veh)
+										TriggerServerEvent("vRP_VehicleScrap:getVehPrice", class)
 									end
 
-									local vehicle = GetVehiclePedIsIn(GetPlayerPed(GetPlayerFromServerId(owner)), false)
-									local plate = GetVehicleNumberPlateText(vehicle)
 									if string.sub(plate, 1, 2) == "P " then
-										TriggerServerEvent('scrap:Mechanic', "scrap:Success") -- PLAYER VEHICLE
-				            				else
-										TriggerServerEvent('scrap:Mechanic', "scrap:SuccessNPC") -- NPC VEHICLE
+										TriggerServerEvent('vRP_VehicleScrap:Mechanic', "vRP_VehicleScrap:Success") -- PLAYER VEHICLE
+									else
+										TriggerServerEvent('vRP_VehicleScrap:Mechanic', "vRP_VehicleScrap:SuccessNPC") -- NPC VEHICLE
 									end
-							    	end
-                            				else
+								end
+							else
 								TriggerEvent("pNotify:SendNotification",{text = "You need to be the driver of the vehicle!",type = "error",timeout = (1500),layout = "centerRight",queue = "global",animation = {open = "gta_effects_fade_in", close = "gta_effects_fade_out"}})
 							end
 						end
@@ -80,21 +73,21 @@ end)
       -- >> SELL FUNCTION
 --------------------------------------------------------------------
 
-RegisterNetEvent("scrap:SuccessNPC")
-AddEventHandler("scrap:SuccessNPC", function() -- NPC VEHICLE
+RegisterNetEvent("vRP_VehicleScrap:SuccessNPC")
+AddEventHandler("vRP_VehicleScrap:SuccessNPC", function() -- NPC VEHICLE
 	TriggerEvent("pNotify:SendNotification",{text = "You have scrapped the vehicle for 1000$",type = "success",timeout = (3500),layout = "centerRight",queue = "global",animation = {open = "gta_effects_fade_in", close = "gta_effects_fade_out"}})
 	TriggerServerEvent("scrap:SellVehicle", 1000)
 	local vehicle = SetEntityAsMissionEntity(GetVehiclePedIsIn(GetPlayerPed(-1)), true, true)
 	DeleteEntity(vehicle)
 end)
 
-RegisterNetEvent("scrap:Success")
-AddEventHandler("scrap:Success", function() -- PLAYER VEHICLE
+RegisterNetEvent("vRP_VehicleScrap:Success")
+AddEventHandler("vRP_VehicleScrap:Success", function() -- PLAYER VEHICLE
 	if vehPrice == nil then
 		TriggerEvent("pNotify:SendNotification",{text = "You can not sell this vehicle",type = "success",timeout = (3000),layout = "centerRight",queue = "global",animation = {open = "gta_effects_fade_in", close = "gta_effects_fade_out"}})
 	else
 		TriggerEvent("pNotify:SendNotification",{text = "You have scrapped the vehicle for "..modPrice.."$",type = "success",timeout = (3500),layout = "centerRight",queue = "global",animation = {open = "gta_effects_fade_in", close = "gta_effects_fade_out"}})
-		TriggerServerEvent("scrap:SellVehicle", modPrice)
+		TriggerServerEvent("vRP_VehicleScrap:SellVehicle", modPrice)
 		local vehicle = SetEntityAsMissionEntity(GetVehiclePedIsIn(GetPlayerPed(-1)), true, true)
 		DeleteEntity(vehicle)
 		vehPrice = nil
@@ -116,16 +109,11 @@ end)
       -- >> BLIPS
 --------------------------------------------------------------------
 
-Citizen.CreateThread(function()
-	local loadedBlip = false
 
-	while true do
-		Citizen.Wait(0)
-		local x,y,z = table.unpack(GetEntityCoords(GetPlayerPed(-1), true))
-	
-		--load blip
-		if not loadedBlip then 
-		  for i = 1, #scrapyards do
+local firstspawn = 0
+AddEventHandler('playerSpawned', function(spawn)
+	if firstspawn == 0 then
+		for i = 1, #scrapyards do
 			garageCoords = scrapyards[i]
 			stationBlip = AddBlipForCoord(garageCoords[1], garageCoords[2], garageCoords[3])
 			SetBlipSprite(stationBlip, 467)
@@ -137,11 +125,10 @@ Citizen.CreateThread(function()
 			BeginTextCommandSetBlipName("String")
 			AddTextComponentString("Scrap")
 			EndTextCommandSetBlipName(stationBlip)
-			loadedBlip = true
-          end
-         end
+			firstspawn = 1
 		end
-	  end)
+	end
+end)
 
 --------------------------------------------------------------------
 --------------------------------------------------------------------
@@ -153,7 +140,7 @@ Citizen.CreateThread(function()
       -- >> DRAW FUNCTION
 --------------------------------------------------------------------
 
-function vehSR_drawTxt(text,font,centre,x,y,scale,r,g,b,a)
+function drawTxt(text,font,centre,x,y,scale,r,g,b,a)
 	SetTextFont(6)
 	SetTextProportional(6)
 	SetTextScale(scale/1.0, scale/1.0)
@@ -170,24 +157,3 @@ end
 
 --------------------------------------------------------------------
 --------------------------------------------------------------------
-
-
-
-
---------------------------------------------------------------------
-      -- >> ROUND
---------------------------------------------------------------------
-
-function round(num, numDecimalPlaces)
-	local mult = 10^(numDecimalPlaces or 0)
-	return math.floor(num * mult + 0.5) / mult
-end
-
---------------------------------------------------------------------
---------------------------------------------------------------------
-
-
-
-
-
-
